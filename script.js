2cf1a7eec96d870a24e82753cd98c4d979019738
SerialEditor <refservicesamara@yandex.ru>
2022-12-04 16:46:30 +0300

    Added slider structure

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 let viewierList = document.querySelector('.images-viewier__list');
-let viewierButtonBar = document.querySelector('.slider__button-bar');
-let buttonBack = viewierButtonBar.querySelector('.button--back');
-let buttonForward = viewierButtonBar.querySelector('.button--forward');
+let viewierItems = viewierList.children;
+
+let slider = document.querySelector('.slider');
+let sliderList = slider.querySelector('.slider__images-list');
+let sliderButtonBar = slider.querySelector('.slider__button-bar');
+let buttonBack = sliderButtonBar.querySelector('.button--back');
+let buttonForward = sliderButtonBar.querySelector('.button--forward');
+
+for (let item of viewierItems) {
+    let clonedItem = item.cloneNode(true);
+    clonedItem.classList.remove('images-viewier__item');
+    clonedItem.classList.add('slider__images-item');
+    sliderList.appendChild(clonedItem);
+}
+
+let sliderItems = sliderList.children; 
+let movementCounter = 0;
 
 buttonBack.addEventListener('click', function () {
     viewierList.scrollTo(0, 0);
+    for (let item of sliderItems) {
+        item.style.transform = 'translateX(0)';
+    }
 });
 
 buttonForward.addEventListener('click', function () {
-    viewierList.scrollTo(viewierList.scrollWidth, 0)
+    viewierList.scrollTo(viewierList.scrollWidth, 0);
+    movementCounter++;
+    let movementValue = movementCounter * 100;
+    for (let item of sliderItems) {
+        item.style.transform = `translateX(-${movementValue}%)`;
+    }
 });
+
+
 
